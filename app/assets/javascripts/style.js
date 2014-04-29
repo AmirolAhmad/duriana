3a8f0855d0b079f09e1f3d1296a3da6683d9f968
AmirolAhmad <9w2pdf@gmail.com>
2014-04-29 14:52:57 +0800

    remove all

--- app/assets/javascripts/style.js
+++
@@ -1,9 +0,0 @@
-function PhoneBook($scope) {
-  $scope.users = [];
- 
-  $scope.addName = function() {
-    $scope.users.push({name:$scope.userName, phone:$scope.userPhone});
-    $scope.userName = '';
-    $scope.userPhone = '';
-  };
-}
--- app/models/phone.rb
+++
@@ -1,4 +0,0 @@
-class Phone < ActiveRecord::Base
-	validates :name, presence: true
-	validates :phone, presence: true, numericality: { only_integer: true }
-end
